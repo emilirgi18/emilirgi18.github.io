49375a5c62676d228f4f6c972b25a245649c99de
Emil Irgi Fathurachmat <emil.irgi.fathurachmat@mni.thm.de>
2019-06-06 20:04:54 +0200

    edited PWA

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,59 @@
-// service-worker.js
+---
+layout: null
+---
+var urlsToCache = [];
 
-// set names for both precache & runtime cache
-workbox.core.setCacheNameDetails({
-    prefix: 'Animus',
-    suffix: 'v1',
-    precache: 'precache',
-    runtime: 'runtime-cache'
+var CACHE_NAME = 'sakb-v1';
+
+ urlsToCache.push("/style.css")
+ urlsToCache.push("/sw.js")
+// Cache posts
+// Limits the number of posts that gets cached to 3
+// Reads a piece of front-matter in each post that directs the second loop to the folder where the assets are held
+{% for post in site.posts %}
+  urlsToCache.push("{{ post.url }}")
+  {% for file in site.static_files %}
+    {% if file.path contains post.assets %}
+      urlsToCache.push("{{ file.path }}")
+    {% endif %}
+  {% endfor %}
+{% endfor %}
+
+// Cache pages
+// Do nothing if it's either an AMP page (as these are served via Googles cache) or the blog page
+// Fallback to the offline pages for these
+{% for page in site.html_pages %}
+  {% if page.path contains 'amp-html' or page.path contains 'blog' %}
+  {% else if %}
+    urlsToCache.push("{{ page.url }}")
+  {% endif %}
+{% endfor %}
+
+// Cache assets
+// Removed assets/posts because I only want assets from the most recent posts getting cached
+{% for file in site.static_files %}
+    {% if file.extname == '.js' or file.extname == '.css' or file.extname == '.jpg' or file.extname == '.png' or file.extname == '.json' %}
+      urlsToCache.push("{{ file.path }}")
+    {% endif %}
+{% endfor %}
+
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-// let Service Worker take control of pages ASAP
-workbox.skipWaiting();
-workbox.clientsClaim();
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, {cacheName:CACHE_NAME,ignoreVary:true}).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
 
-// let Workbox handle our precache list
-workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
-// use `networkFirst` strategy for `*.html`, like all my posts
-workbox.routing.registerRoute(
-    /\.html$/,
-    workbox.strategies.networkFirst()
-);
-
-// use `cacheFirst` strategy for images
-workbox.routing.registerRoute(
-    /assets\/(img|icons)/,
-    workbox.strategies.cacheFirst()
-);
-
-// third party files
-workbox.routing.registerRoute(
-    /^https?:\/\/cdn.staticfile.org/,
-    workbox.strategies.staleWhileRevalidate()
-);
